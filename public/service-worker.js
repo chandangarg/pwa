664b82014c74edad4847836d5dcb4390b304ab8b
chandangarg <cgarg3@gmail.com>
2023-03-26 11:08:12 +0530

    file updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,7 @@
+self.skipWaiting();
+
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+    console.log('fetch function executed')
 });
 
 self.addEventListener('install', function(event) {
